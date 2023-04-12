657e33aceca465c16ccccf5f1612cda9e0064029
TommyLiang1 <tommyliang131@gmail.com>
2023-04-11 22:06:07 -0400

    Add project files

---
+++ ChattingAPI/ChattingAPI/Models/ChattingAPIDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ChattingAPI.Models
+{
+    public class ChattingAPIDBContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public ChattingAPIDBContext(DbContextOptions<ChattingAPIDBContext> options, IConfiguration configuration) : base (options)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            var connectionString = Configuration.GetConnectionString("UserDataService");
+            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+
+        public DbSet<User> User { get; set; } = null!;
+        public DbSet<Post> Post { get; set; } = null!;
+        //public DbSet<Email> Emails { get; set; } = null!;
+    }
+}
---
+++ ChattingAPI/ChattingAPI/Models/Post.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ChattingAPI.Models
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string PostDescription { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
