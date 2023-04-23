5cfbaac5d60f477709ab21d9e23237e3edb6c185
TommyLiang1 <tommyliang131@gmail.com>
2023-04-23 15:58:27 -0400

    Uncomment cors

--- ChattingAPI/Program.cs
+++ ChattingAPI/Program.cs
@@ -1,20 +1,20 @@
 using Microsoft.EntityFrameworkCore;
 using ChattingAPI.Models;
 
-var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+// var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ChattingAPIDBContext>();
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: MyAllowSpecificOrigins, policy =>
-    {
-        policy.WithOrigins("http://localhost:3000");
-    });
-});
+// builder.Services.AddCors(options =>
+// {
+//     options.AddPolicy(name: MyAllowSpecificOrigins, policy =>
+//     {
+//         policy.WithOrigins("http://localhost:3000");
+//     });
+// });
 
 var app = builder.Build();
 
